d88d9bd8f5aa888992d4cff1b60100537f765c4e
Dieg0Code <diegoobando20@gmail.com>
2025-06-19 10:54:25 -0400

    Fix: Convert submodules to regular folders

---
+++ my-dashboard/src/app/pokemons/index.ts
@@ -0,0 +1,6 @@
+
+
+export { PokemonsGrid } from './components/PokemonsGrid';
+
+export type { PokemonResponse } from './interfaces/pokemon-response';
+export type { SimplePokemon } from './interfaces/simple-pokemon';
